92242fe34a5902137816f52bd3dfa7adc02882e7
Vladimir Voytenko <voldemar.krs@gmail.com>
2017-09-01 15:31:26 +0700

    add to admin

--- medical_record/admin.py
+++ medical_record/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Record, Department, Doctor
 
-# Register your models here.
+admin.site.register(Record)
+admin.site.register(Doctor)
+admin.site.register(Department)
